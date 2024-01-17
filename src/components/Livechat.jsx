64517d2f29b51542ae79074ce7f41a1d5d599e67
louis frederic schefer <lf.schefer@gmail.com>
2024-01-17 22:05:42 +0100

    auto scroll ok

--- src/components/Livechat.jsx
+++ src/components/Livechat.jsx
@@ -4,8 +4,14 @@ import './Livechat.css'
 export default function Livechat(props) {
 
   const tchat = props.messages.map((message,index) => {
-    return <div key={index}><p style={{'color':`${message.userColor}`}}>{message.user}:</p> <p>{message.message}</p></div>
+    return <div key={index}><p style={{'color':`${message.userColor === null ? '#FF7F50' : message.userColor }`}}>{message.user}:</p> <p>{message.message}</p></div>
   })
+
+
+  React.useEffect(()=> {
+      let objDiv = document.querySelector(".feed");
+      objDiv.scrollTop = objDiv.scrollHeight;
+  },[props.messages])
 
   return (
     <div className='chat-container'>
