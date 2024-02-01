a38549d3d62a63a69a398f34f5ea96a40f5e44ee
louis frederic schefer <lf.schefer@gmail.com>
2024-02-01 05:28:53 +0100

    basic read me

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,9 @@ import './Header.css'
 export default function Header(props) {
 
   return (
-    <header className='mb-4'>
-      <h1>Test app</h1>
+    <header className='mb-4 header'>
+      <h4>Twitch chat word tracker</h4>
+      <p>v 0.8</p>
     </header>
   )
 }
