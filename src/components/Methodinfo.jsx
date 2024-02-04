cee0d48caad178f28b9b58134a20c310405b7b9f
louis frederic schefer <lf.schefer@gmail.com>
2024-02-04 18:34:08 +0100

    info txt start

--- src/components/Methodinfo.jsx
+++ src/components/Methodinfo.jsx
@@ -5,10 +5,34 @@ import { faInfo } from '@fortawesome/free-solid-svg-icons';
 
 export default function Methodinfo(props) {
 
-  const infoText = <p>test</p>
+  let exempleText = "";
+  let explanation = ""
 
-  const info = <div className='info-card'>
-    {infoText}
+  switch (props.checker) {
+    case "1":
+      explanation = <p className='explanation'>Strick mode, exact match only.</p>
+      exempleText = <p>Exemple input: "Harry Poter"</p>
+      break;
+    case "2":
+      explanation = <p className='explanation'>No capital mode, capitalize and none capitalize letters are the same.</p>
+      exempleText = <p>Exemple input: "Harry Poter"</p>
+      break;
+    case "3":
+      explanation = <p className='explanation'>No space mode, space do not count.</p>
+      exempleText = <p>Exemple input: "Harry Poter"</p>
+      break;
+    case "4":
+      explanation = <p className='explanation'>No cap and space mode, Capitalize letter and space don't count.</p>
+      exempleText = <p>Exemple input: "Harry Poter"</p>
+      break;
+
+    default:
+      break;
+  }
+
+  const info = <div className='info-card card'>
+    {explanation}
+    {exempleText}
   </div>
 
 
