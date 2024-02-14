5dae9acb31545e2b5c3f96ffc38e6427413eea70
louis frederic schefer <lf.schefer@gmail.com>
2024-02-14 04:22:04 +0100

    score method to imp

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -3,18 +3,20 @@ import "./Score.css";
 
 export default function Score(props) {
 
-  console.log(props.score)
 
-  const displayScore = props.score.map((score)=>{
-    return <div>{score}</div>
-  })
+  const test = Object.entries(props.score).sort((a,b)=>{return b[1].score-a[1].score})
+  console.log(props.score)
+  console.log(test)
+
+  // const displayScore = props.score.map((score)=>{
+  //   return <div>{score}</div>
+  // })
 
 
   return (
     <div className="winners-container">
     <h5>Score:</h5>
     <div className="winners-list">
-      {displayScore}
     </div>
   </div>
   )
