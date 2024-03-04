a6a447fb165bd12def5c56448cb5e5ef9f2a27cf
louis frederic schefer <lf.schefer@gmail.com>
2024-03-04 23:12:02 +0100

    test +

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders page', () => {
   render(<App />);
   const header = screen.getByText('Twitch chat word tracker');
+  const wordTracker = screen.getByText('Words to track:');
   expect(header).toBeInTheDocument();
+  expect(wordTracker).toBeInTheDocument();
 });
