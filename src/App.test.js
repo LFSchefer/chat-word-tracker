334d32c6fa2f6c42d5ec9db9e3d70633a041a6bf
louis frederic schefer <lf.schefer@gmail.com>
2024-03-06 09:23:33 +0100

    test 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders page', () => {
   render(<App />);
@@ -7,4 +8,21 @@ test('renders page', () => {
   const wordTracker = screen.getByText('Words to track:');
   expect(header).toBeInTheDocument();
   expect(wordTracker).toBeInTheDocument();
+});
+
+test('enter a chanel chanel', () => {
+  render(<App />);
+  userEvent.type(screen.getByPlaceholderText('Ex: mistermv'), 'jossBergia');
+  expect(screen.getByPlaceholderText("Ex: mistermv")).toHaveValue("jossBergia");
+});
+
+test('connect to a chanel', () => {
+  render(<App />);
+  userEvent.type(screen.getByPlaceholderText('Ex: mistermv'), 'jossBergia');
+  expect(screen.getByPlaceholderText("Ex: mistermv")).toHaveValue("jossBergia");
+  const connectBtn = screen.getByText(/Connect/i);
+  fireEvent.click(connectBtn);
+  const liveTchat = screen.getByText(/Live tchat:/i);
+  expect(liveTchat).toBeInTheDocument();
+  expect(liveTchat).toHaveTextContent(/jossBergia/i)
 });
